f05911f11bcf2eb415df36c6759e985254005036
trystandj <trystandj@gmail.com>
2024-11-18 16:06:41 -0500

    major changes to app

--- My_Stuff/searcher/main.go
+++ My_Stuff/searcher/main.go
@@ -10,39 +10,64 @@ import (
 )
 
 func main() {
-
-	var directory string
-
-	fmt.Print("Enter you directory: ")
-
 	reader := bufio.NewReader(os.Stdin)
-	directory, _ = reader.ReadString('\n')
-	directory = strings.TrimSpace(directory)
-	folderSearch := strings.ReplaceAll(directory, "\\", "/")
-	println(folderSearch)
 
-	// folderSearch := "C:/Program Files (x86)/DocLogic"
-	fileEnd := ".ini"
-	fileName := "rdi"
-	removeName := "copy old 2"
-	searchContent := "prodoc 2020doc statements greenbar mysql"
+	// Get Notepad++ executable path
+	fmt.Print("Enter the EXE path of Notepad++: ")
+	cleanPath, _ := reader.ReadString('\n')
+	cleanPath = strings.TrimSpace(strings.ReplaceAll(cleanPath, `\`, "/"))
+	cleanPath = strings.ReplaceAll(cleanPath, `"`, "")
 
-	//search.go brings back a file list with the specified file end
-	files := file.SearchFiles(folderSearch, fileEnd)
-	println()
-	// filter brings back a list of files with the containg search content
-	filteredFiles := file.FilterFilesLineByLine(files, searchContent)
-	println()
-	filteredFilesNames, err := file.FilterFilesName(filteredFiles, fileName)
-	if err != nil {
-		fmt.Println("Error:", err)
+	for {
+		// Get directory input from the user
+		fmt.Print("Enter your directory: ")
+		directory, _ := reader.ReadString('\n')
+		folderSearch := strings.ReplaceAll(strings.TrimSpace(directory), "\\", "/")
+		folderSearch = strings.ReplaceAll(folderSearch, `"`, "")
+		println(folderSearch)
+
+		// Define search parameters
+		fileEnd := ".ini"
+		fileName := "rdi"
+		removeName := "copy old 2 sample demo"
+		searchContent := "prodoc= 2020doc= statements= greenbar= MySQLConfig="
+
+		// Search for files with the specified file extension
+		files := file.SearchFiles(folderSearch, fileEnd)
+		println()
+
+		// Filter files based on content
+		filteredFiles := file.FilterFilesLineByLine(files, searchContent)
+		println()
+
+		// Filter files based on the name
+		filteredFilesNames, err := file.FilterFilesName(filteredFiles, fileName)
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+		println()
+
+		// Strip specific parts of the file name
+		strippedFiles, err := file.StripName(filteredFilesNames, removeName)
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+
+		// Open filtered files in Notepad++
+		opener.OpenNotepad(strippedFiles, cleanPath)
+		println()
+		fmt.Println("Please check for any missed files.")
+
+		// Ask the user if they want to search another directory
+		fmt.Print("Would you like to search another directory? (yes/no): ")
+		choice, _ := reader.ReadString('\n')
+		choice = strings.TrimSpace(strings.ToLower(choice))
+		// && is like the or
+		if choice != "yes" && choice != "y" {
+			fmt.Println("Exiting the program.")
+			break
+		}
+
+		println()
 	}
-	println()
-	strippedFiles, err := file.StripName(filteredFilesNames, removeName)
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-	opener.OpenNotepad(strippedFiles)
-	println()
-	fmt.Println("Please check for any missed files.")
 }
--- My_Stuff/searcher/opener/notepad.go
+++ My_Stuff/searcher/opener/notepad.go
@@ -6,23 +6,21 @@ import (
 	"os/exec"
 )
 
-func OpenNotepad(files []string) {
+func OpenNotepad(files []string, exePath string) {
+
 	fmt.Println("\nOpening files in Notepad...")
 
 	for _, file := range files {
-		// Check if the file exists
+
 		if _, err := os.Stat(file); os.IsNotExist(err) {
 			fmt.Printf("File does not exist: %s\n", file)
 			continue
 		}
 
-		// Use the default notepad command available in the system's PATH
-		cmd := exec.Command("C:\\Program Files (x86)\\Notepad++\\notepad++.exe", file)
+		cmd := exec.Command(exePath, file)
 
-		// Start the Notepad process to open the file
 		err := cmd.Start()
 
-		// Error handling to log any issues when opening the file
 		if err != nil {
 			fmt.Printf("Error opening file %s in Notepad: %v\n", file, err)
 		} else {
